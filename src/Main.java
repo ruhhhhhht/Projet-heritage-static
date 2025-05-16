0b06bb2148f069a36436a34b315cf735a826a75b
coulibaly <coulibalyruth87@gmail.com>
2025-05-16 11:55:10 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        personne personne1 = new personne();
+        personne personne2 = new personne();
+        personne personne3 = new personne();
+        personne personne4 = new personne();
+        //affichez les valeurs des deux compteurs
+        System.out.println("("+ personne4.nbLocal+","+personne.nbInstances+")");
+        }
+
+}
---
+++ src/personne.java
@@ -0,0 +1,8 @@
+public class personne {
+    public static int nbInstances;
+    public int nbLocal;
+    public personne() {
+        nbInstances++;
+        nbLocal++;
+    }
+}
